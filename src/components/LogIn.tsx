4735b6307cce29de76a3c474c2d054717ea1e913
Mafifa <charlysmafifa@gmail.com>
2024-08-23 23:07:35 -0400

    Codigo formateado

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,87 +1,140 @@
-import React, { useState } from 'react'
-import Spinner from './Spinner'
+import React, { useState } from "react";
+import Spinner from "./Spinner";
 
 function LogIn () {
-  const [usuario, setUsuario] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [valido, setValido] = useState(false)
-  const [validoPassword, setValidoPassword] = useState(false)
-  const [mostrarModal, setMostrarModal] = useState(false)
-  const [visible, setVisible] = useState(false)
-  const [error, setError] = useState('')
-  const [Cargar, setCargar] = useState(false)
-
+  const [usuario, setUsuario] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [valido, setValido] = useState(false);
+  const [validoPassword, setValidoPassword] = useState(false);
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [error, setError] = useState("");
+  const [Cargar, setCargar] = useState(false);
 
   const handleUsuario = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newQuery = e.target.value
-    setUsuario(newQuery)
+    const newQuery = e.target.value;
+    setUsuario(newQuery);
     if (newQuery.length > 4) {
-      setValido(true)
+      setValido(true);
     } else {
-      setValido(false)
+      setValido(false);
     }
-  }
+  };
 
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newQuery = e.target.value
-    setPassword(newQuery)
+    const newQuery = e.target.value;
+    setPassword(newQuery);
     if (newQuery.length > 4) {
-      setValidoPassword(true)
+      setValidoPassword(true);
     } else {
-      setValidoPassword(false)
+      setValidoPassword(false);
     }
 
-    if (newQuery === '') {
-      setError('Contraseña es requerida')
+    if (newQuery === "") {
+      setError("Contraseña es requerida");
     } else {
-      setError('')
+      setError("");
     }
-  }
+  };
 
   const handleClick = () => {
-    setCargar(true)
+    setCargar(true);
 
     setTimeout(() => {
-      setCargar(false)
-    }, 1000)
+      setCargar(false);
+    }, 1000);
 
     if (valido) {
-      setMostrarModal(true)
+      setMostrarModal(true);
     }
-  }
+  };
 
   const cerrarModal = () => {
-    setMostrarModal(false)
-  }
+    setMostrarModal(false);
+  };
 
   const handleSend = () => {
     // Aqui ira la logica del envio
-  }
+  };
 
   return (
-    <div className='grid grid-cols-1 items-center gap-6 py-6'>
-      <div className='grid grid-cols-1 items-center bg-[#0000000a] rounded-sm pt-3 px-3 border-b-2 border-solid hover:border-[#bb1b47]'>
-        <label className='text-sm text-black/50' htmlFor="usuario">Usuario *</label>
-        <input value={usuario} onChange={handleUsuario} className='bg-transparent w-full' type="text" name="usuario" id="usuario" />
+    <div className="grid grid-cols-1 items-center gap-6 py-6">
+      <div className="grid grid-cols-1 items-center bg-[#0000000a] rounded-sm pt-3 px-3 border-b-2 border-solid hover:border-[#bb1b47]">
+        <label className="text-sm text-black/50" htmlFor="usuario">
+          Usuario *
+        </label>
+        <input
+          value={usuario}
+          onChange={handleUsuario}
+          className="bg-transparent w-full"
+          type="text"
+          name="usuario"
+          id="usuario"
+        />
       </div>
-      <button disabled={!valido} onClick={handleClick} className={`${valido ? 'bg-[#0067b1] cursor-pointer text-white/90' : 'bg-[#0000001f] cursor-default text-black/30'}  w-1/2 mx-auto rounded-sm shadow-md text-sm p-3`}>Entrar</button>
+      <button
+        disabled={!valido}
+        onClick={handleClick}
+        className={`${valido
+            ? "bg-[#0067b1] cursor-pointer text-white/90"
+            : "bg-[#0000001f] cursor-default text-black/30"
+          }  w-1/2 mx-auto rounded-sm shadow-md text-sm p-3`}
+      >
+        Entrar
+      </button>
 
       {/* Modal */}
       {mostrarModal && (
-        <div className='fixed inset-0 bg-black/30 flex justify-center items-center'>
-          <div className='bg-white p-6 rounded-sm shadow-black/30 shadow-xl grid grid-cols-1 lg:px-24 sm:px-16'>
-            <p className='text-base text-center font-extrabold mb-4 text-[#0067b1]'>Introduce tu contraseña</p>
-            <div className='mb-10 flex flex-col bg-[#0000000a] rounded-sm pt-3 px-3 border-b-2 border-solid hover:border-[#bb1b47]'>
-              <label className='text-sm text-black/50 text-start' htmlFor="usuario">Contraseña *</label>
-              <button style={{ boxSizing: 'border-box' }} className='ml-auto -my-2' onClick={() => setVisible(!visible)} >
-                <img src='./images/eye.png' alt='mostrar - ocultar' />
+        <div className="fixed inset-0 bg-black/30 flex justify-center items-center">
+          <div className="bg-white p-6 rounded-sm shadow-black/30 shadow-xl grid grid-cols-1 lg:px-24 sm:px-16">
+            <p className="text-base text-center font-extrabold mb-4 text-[#0067b1]">
+              Introduce tu contraseña
+            </p>
+            <div className="mb-10 flex flex-col bg-[#0000000a] rounded-sm pt-3 px-3 border-b-2 border-solid hover:border-[#bb1b47]">
+              <label
+                className="text-sm text-black/50 text-start"
+                htmlFor="usuario"
+              >
+                Contraseña *
+              </label>
+              <button
+                style={{ boxSizing: "border-box" }}
+                className="ml-auto -my-2"
+                onClick={() => setVisible(!visible)}
+              >
+                <img src="./images/eye.png" alt="mostrar - ocultar" />
               </button>
-              <input value={password} onChange={handlePassword} className='bg-transparent w-max' type={visible ? 'text' : 'password'} name="password" id="password" />
+              <input
+                value={password}
+                onChange={handlePassword}
+                className="bg-transparent w-max"
+                type={visible ? "text" : "password"}
+                name="password"
+                id="password"
+              />
             </div>
-            {error && (<span className='text-[#bb1b47] text-end text-sm -mt-6 md:-mt-8'>Contraseña es <strong>requerida</strong></span>)}
-            <div className='grid grid-cols-1 md:grid-cols-2 items-center text-center gap-y-4 mt-3'>
-              <button disabled={!validoPassword} onClick={handleSend} className={`${validoPassword ? 'bg-[#0067b1] cursor-pointer text-white/90' : 'bg-[#0000001f] cursor-default text-black/30'}  w-1/2 md:w-10/12 mx-auto rounded text-sm py-2 px-4`}>Continuar</button>
-              <button onClick={cerrarModal} className='text-sm bg-[#0067b1] text-white/90 py-2 px-4 rounded w-1/2 md:w-10/12 mx-auto'>Cancelar</button>
+            {error && (
+              <span className="text-[#bb1b47] text-end text-sm -mt-6 md:-mt-8">
+                Contraseña es <strong>requerida</strong>
+              </span>
+            )}
+            <div className="grid grid-cols-1 md:grid-cols-2 items-center text-center gap-y-4 mt-3">
+              <button
+                disabled={!validoPassword}
+                onClick={handleSend}
+                className={`${validoPassword
+                    ? "bg-[#0067b1] cursor-pointer text-white/90"
+                    : "bg-[#0000001f] cursor-default text-black/30"
+                  }  w-1/2 md:w-10/12 mx-auto rounded text-sm py-2 px-4`}
+              >
+                Continuar
+              </button>
+              <button
+                onClick={cerrarModal}
+                className="text-sm bg-[#0067b1] text-white/90 py-2 px-4 rounded w-1/2 md:w-10/12 mx-auto"
+              >
+                Cancelar
+              </button>
             </div>
           </div>
         </div>
@@ -89,13 +142,12 @@ function LogIn () {
 
       {/* Mostrar Cargar */}
       {Cargar && (
-        <div className='fixed inset-0 bg-white/50 flex justify-center items-center'>
+        <div className="fixed inset-0 bg-white/50 flex justify-center items-center">
           <Spinner />
         </div>
       )}
-
     </div>
-  )
+  );
 }
 
-export default LogIn
+export default LogIn;
--- src/components/SendCodigo.tsx
+++ src/components/SendCodigo.tsx
@@ -1,56 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import RoundedInput from './RoundedInput'
-import Spinner from './Spinner'
+import React, { useEffect, useState } from "react";
+import RoundedInput from "./RoundedInput";
+import Spinner from "./Spinner";
 
 function SendCodigo () {
-  const [codigo, setCodigo] = useState('')
-  const [valido, setValido] = useState(false)
-  const [mostrarModal, setMostrarModal] = useState(false)
+  const [codigo, setCodigo] = useState("");
+  const [valido, setValido] = useState(false);
+  const [mostrarModal, setMostrarModal] = useState(false);
 
   const handleCodigo = (value: string) => {
-    setCodigo(value)
-  }
+    setCodigo(value);
+  };
 
   useEffect(() => {
     if (codigo.length > 5) {
-      setValido(true)
+      setValido(true);
     } else {
-      setValido(false)
+      setValido(false);
     }
-  }, [codigo])
-
+  }, [codigo]);
 
   const handleSend = () => {
     // Aqui ira la logica del envio
     console.log(codigo);
 
-    setMostrarModal(true)
+    setMostrarModal(true);
     setTimeout(() => {
-      setMostrarModal(false)
+      setMostrarModal(false);
     }, 2000);
-
-  }
+  };
 
   return (
-    <div className='grid grid-cols-1 items-center gap-6 py-6'>
-      <div className='flex items-center justify-center'>
+    <div className="grid grid-cols-1 items-center gap-6 py-6">
+      <div className="flex items-center justify-center">
         <RoundedInput onValuesChange={handleCodigo} />
       </div>
       <button
         disabled={!valido}
         onClick={handleSend}
-        className={`${valido ? 'bg-[#0067b1] cursor-pointer text-white/90' : 'bg-[#0000001f] cursor-default text-black/30'}  w-1/2 mx-auto rounded-sm shadow-md text-sm p-3`}>
+        className={`${
+          valido
+            ? "bg-[#0067b1] cursor-pointer text-white/90"
+            : "bg-[#0000001f] cursor-default text-black/30"
+        }  w-1/2 mx-auto rounded-sm shadow-md text-sm p-3`}
+      >
         Entrar
       </button>
 
       {/* Modal */}
       {mostrarModal && (
-        <div className='fixed inset-0 bg-white/50 flex justify-center items-center'>
+        <div className="fixed inset-0 bg-white/50 flex justify-center items-center">
           <Spinner />
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default SendCodigo
+export default SendCodigo;
